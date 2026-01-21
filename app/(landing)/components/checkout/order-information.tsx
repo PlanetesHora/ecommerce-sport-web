37b6315909aa28a5471aa9a3b859b6e3750d5900
Rizka Rahmadina <hi.rizkarahmadina@gmail.com>
2026-01-22 02:53:01 +0700

    fix: final typescript build errors and order status logic

--- app/(landing)/components/checkout/order-information.tsx
+++ app/(landing)/components/checkout/order-information.tsx
@@ -13,11 +13,13 @@ const OrderInformation = ({ formData, setFormData }: TOrderInformation) => {
   const handleInputChange = (
     e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
   ) => {
-    setFormData((prev) => ({ 
-      ...prev, 
+    setFormData?.((prev) => ({ 
+    ...prev, 
       [e.target.name]: e.target.value 
     }));
   };
+
+  if (!formData) return null;
 
   return (
     <CardWithHeader title="Order Information">
@@ -50,7 +52,7 @@ const OrderInformation = ({ formData, setFormData }: TOrderInformation) => {
             placeholder="Type your whatsapp number"
             id="customerContact"
             name="customerContact"
-            value={formData.customerContact}
+            value={formData?.customerContact ?? ""}
             onChange={handleInputChange}
           />
         </div>
@@ -66,7 +68,7 @@ const OrderInformation = ({ formData, setFormData }: TOrderInformation) => {
             name="customerAddress"
             placeholder="Type your shipping address"
             rows={7}
-            value={formData.customerAddress}
+            value={formData?.customerAddress ?? ""}
             onChange={handleInputChange}
           />
         </div>
