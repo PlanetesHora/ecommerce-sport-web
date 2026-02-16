91011a4162071ec8c607a7009a55626f394132c3
Rizka Rahmadina <hi.rizkarahmadina@gmail.com>
2026-02-17 02:21:07 +0700

    config: allow images from cloudinary

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
         hostname: "sporton-backend-production-b563.up.railway.app",
         pathname: "/uploads/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com", 
+        pathname: "/**",
+      },
     ],
   },
 };
