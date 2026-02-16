adf176c97059ab9d1ff8d8c89a159e7b170f1d30
Rizka Rahmadina <hi.rizkarahmadina@gmail.com>
2026-02-17 01:41:20 +0700

    feat: connect frontend to railway backend

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "be-sporton.agunacourse.com",
+        hostname: "sporton-backend-production-b563.up.railway.app",
         pathname: "/uploads/**",
       },
     ],
